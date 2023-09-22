8c80cd2019c886ce4a1f17212245715b04fb6af1
sabaaa96 <saba.mzd@gmail.com>
2023-09-22 10:36:55 +0200

    Initial commit

---
+++ 100 days python/day7-hangman.py
@@ -0,0 +1,91 @@
+import random
+
+
+stages = ['''
+   +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''', '''
+   +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+ +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+   +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+   +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''']
+
+
+word_list = ["kaffee", "strasse", "milk"]
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+lives = 6
+
+print(f"Psst, the solution is {chosen_word}.")
+
+display = []
+for _ in range(word_length):
+    display += "_"
+
+end_of_game = False
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    for position in range(word_length):
+        letter = chosen_word[position]
+        #print(f"Current position: {position}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
+
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+
+
+    print(" ".join(display))
+
+    if "_" not in display:
+        end_of_game = True
+        print("You win!")
+
+    print(stages[lives])
